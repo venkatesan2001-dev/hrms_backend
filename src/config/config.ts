17adb8a33e380641fd1a4c4d71aea36a8f88339e
Venkatesan K <venkatesan2001official@gmail.com>
2025-08-26 01:03:42 +0530

    Config file added

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,9 +3,10 @@ import * as yaml from 'js-yaml';
 import { join } from 'path';
 
 export default () => {
-    let COMPANYSERVICE_CONFIG_PATH = 
-    './config.yml'
-    return yaml.load(
-        readFileSync(join(COMPANYSERVICE_CONFIG_PATH), 'utf8'),
-    ) as Record<string, any>;
+  let COMPANYSERVICE_CONFIG_PATH = './config.yml';
+  const res = yaml.load(
+    readFileSync(join(COMPANYSERVICE_CONFIG_PATH), 'utf8'),
+  ) as Record<string, any>;
+  console.log('Configuration Loaded:', res);
+  return res;
 };
