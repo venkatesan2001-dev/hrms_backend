fae44f64ac15bc2f49ffca187c6d3b4137be3e7e
Venkatesan K <venkatesan2001official@gmail.com>
2025-08-26 00:24:01 +0530

    Transformation to nest js

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/users')
+  getSampleUsers(): any {
+    return [{ name: 'Venkatesan K' }];
+  }
 }
