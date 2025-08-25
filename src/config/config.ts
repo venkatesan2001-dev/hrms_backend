3b8ebed3014158bd89cbeacd03c02f3a0f332bde
Venkatesan K <venkatesan2001official@gmail.com>
2025-08-26 01:18:04 +0530

    Vercel configuration added

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,16 +3,16 @@ import * as yaml from 'js-yaml';
 import { join } from 'path';
 
 export default () => {
-  //   let COMPANYSERVICE_CONFIG_PATH = './config.yml';
-  //   const res = yaml.load(
-  //     readFileSync(join(COMPANYSERVICE_CONFIG_PATH), 'utf8'),
-  //     ) as Record<string, any>;
-  const res = {
-    MONGO_URI:
-      'mongodb+srv://venkatesan2001official:venkatesan2001official@cluster0.we3z6ih.mongodb.net/"',
-    PORT: 8000,
-    HASH_SECRET_KEY: 'HASH_SECRET_KEY',
-  };
+  let COMPANYSERVICE_CONFIG_PATH = './config.yml';
+  const res = yaml.load(
+    readFileSync(join(COMPANYSERVICE_CONFIG_PATH), 'utf8'),
+  ) as Record<string, any>;
+  //   const res = {
+  //     MONGO_URI:
+  //       'mongodb+srv://venkatesan2001official:venkatesan2001official@cluster0.we3z6ih.mongodb.net/"',
+  //     PORT: 8000,
+  //     HASH_SECRET_KEY: 'HASH_SECRET_KEY',
+  //   };
   console.log('Configuration Loaded:', res);
   return res;
 };
