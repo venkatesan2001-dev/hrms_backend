98a65a5634bc60fef4da235b2aec9148a06d2947
Venkatesan K <venkatesan2001official@gmail.com>
2025-08-26 11:08:46 +0530

    config file added

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,10 +3,17 @@ import * as yaml from 'js-yaml';
 import { join } from 'path';
 
 export default () => {
-  let COMPANYSERVICE_CONFIG_PATH = '././././config.yml';
-  const res = yaml.load(
-    readFileSync(join(COMPANYSERVICE_CONFIG_PATH), 'utf8'),
-  ) as Record<string, any>;
-  console.log('res', res);
-  return res;
+  return {
+    MONGO_URI:
+      'mongodb+srv://venkatesan2001official:venkatesan2001official@cluster0.we3z6ih.mongodb.net/',
+    PORT: 8000,
+    HASH_SECRET_KEY: 'HASH_SECRET_KEY',
+    STAGE: 'DEV',
+    JWT_SECRET_KEY: 'JWT_SECRET_KEY',
+    JWT_EXPIRATION_TIME: 3600,
+  };
+  //   let COMPANYSERVICE_CONFIG_PATH = './config.yml';
+  //   return yaml.load(
+  //     readFileSync(join(COMPANYSERVICE_CONFIG_PATH), 'utf8'),
+  //   ) as Record<string, any>;
 };
